afb9298ca343425d73d5d0a2bf60ceaab40a6368
Junior Joanis <junior.joanis@gmail.com>
2024-10-16 13:07:50 +0200

    pwd

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -188,12 +188,14 @@ module Receipts
     end
 
     def default_message(company:)
+      msg = ""
       if company.fetch(:invoice_note, '').present?
-        "#{company.fetch(:invoice_note, '')}<br><br>"
+        msg += "#{company.fetch(:invoice_note, '')}<br><br>"
       end
       if company.fetch(:email,'').present?
-        "#{company.fetch(:contact_text, 'Contact us:')}<color rgb='326d92'><link href='mailto:#{company.fetch(:email,'')}'><b>#{company.fetch(:email,'')}</b></link></color>."
+        msg += "#{company.fetch(:contact_text, 'Contact us:')}<color rgb='326d92'><link href='mailto:#{company.fetch(:email,'')}'><b>#{company.fetch(:email,'')}</b></link></color>."
       end
+      msg
     end
   end
 end
